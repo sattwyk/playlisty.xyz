9307045509144b845f6c403cff784670b06275b0
Sattwik <hellosattwik@gmail.com>
2023-05-11 03:27:02 +0530

    fix docs bug and updated the home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,22 +8,24 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          Finally, a way to manage your YouTube playlists{" "}
+          <br className="hidden sm:inline" />
+          without getting distracted.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+          Playlisty.xyx is the perfect tool for students, professionals, and
+          anyone who wants to stay on track and get things done. It&apos;s free
+          and open source, so you can use it to your heart&apos;s content.
         </p>
       </div>
       <div className="flex gap-4">
         <Link
-          href={siteConfig.links.docs}
+          href={siteConfig.links.twitter}
           target="_blank"
           rel="noreferrer"
           className={buttonVariants({ size: "lg" })}
         >
-          Documentation
+          Twitter
         </Link>
         <Link
           target="_blank"
