e15a79181e9290e536793e7f3b5b421fce23e9a2
Sattwik <hellosattwik@gmail.com>
2023-05-11 03:35:19 +0530

    changed to edge runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
+export const runtime = "edge"
+
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
