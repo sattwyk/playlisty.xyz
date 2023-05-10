ebfd9005d5489d8679b613ca112f9db577a26aad
Sattwik <hellosattwik@gmail.com>
2023-05-11 03:14:07 +0530

    Updated site config and layout

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Playlisty",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Playlisty.xyx is a YouTube playlist manager that helps you stay focused and distraction-free.",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +11,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/sattwyk",
+    github: "https://github.com/sattwyk/playlisty.xyz",
   },
 }
