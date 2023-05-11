6044ade092ef4e01091004bf5a3d974717557b6c
Sattwik <hellosattwik@gmail.com>
2023-05-11 13:04:28 +0530

    removed unused imports form root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import { Metadata } from "next"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { SiteFooter } from "@/components/site-footer"
-import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
